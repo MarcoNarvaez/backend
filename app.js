6e950d5a98291845c1e9ecbcc1eeae07fd346706
Marco Narvaez <nefftep79@gmail.com>
2022-10-24 22:16:56 -0300

    practice

---
+++ app.js
@@ -0,0 +1,4 @@
+const { saludarHolaMundo } = require("./saludos")
+
+//console.log(saludo.saludar(' freeCodeCamp'));
+console.log(saludarHolaMundo());
---
+++ saludos.js
@@ -0,0 +1,12 @@
+function saludar (nombre) {
+    return "hola" + nombre
+}
+
+function saludarHolaMundo() {
+    return 'Hola mundo'
+}
+
+module.exports = {
+    saludar: saludar,
+    saludarHolaMundo: saludarHolaMundo,
+}
