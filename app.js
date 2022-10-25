fde9e37a9db4f7b790d06da444bcec690d3fad69
Marco Narvaez <nefftep79@gmail.com>
2022-10-24 22:17:12 -0300

    practice

--- app.js
+++ app.js
@@ -1,4 +1,35 @@
-const { saludarHolaMundo } = require("./saludos")
+// const { saludarHolaMundo, saludar } = require("./saludos")
 
-//console.log(saludo.saludar(' freeCodeCamp'));
-console.log(saludarHolaMundo());
+// console.log(saludar(' freeCodeCamp'));
+// console.log(saludarHolaMundo());
+
+//Console
+// console.log('Hola, mundo!');
+// console.warn('Ocurrio un error')
+// console.error('Error!!!')
+// console.error(new Error('Ocurrio un error!'))
+
+//Process
+
+// console.log(process);
+// console.log(process.env);
+// console.log(process.argv[2]);
+
+for (let i = 2; i < process.argv.length; i++) {
+    console.log(process.argv[i]);
+}
+
+console.log(process.memoryUsage());
+
+// OS
+
+const os = require('os')
+console.log(os.userInfo())
+
+// Timers
+
+function mostrarTema(tema) {
+    console.log(`Estoy aprendiendo ${tema}`);
+}
+
+setTimeout(mostrarTema, 2000, 'Node.js')
